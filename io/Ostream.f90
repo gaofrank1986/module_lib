390d472b8accad7b67563bae6a0d20393a6417d3
home <home@homedeMacBook-Pro.local>
2016-07-28 14:42:46 +0800

    add name list feature, not completed,tested ok

--- io/Ostream.f90
+++ io/Ostream.f90
@@ -4,10 +4,19 @@ module io
 
     type Ostream
         private
+        ! @var : [port] the port number list
         integer,allocatable :: port(:)
+        ! @var: [header] prompt at beginning
         character(len=:),allocatable :: header
+        ! @var: [namelst] input file names,optional
+        character(len=20),dimension(:),allocatable :: namelst
+        ! number list
+        ! todo name list or a dictionary
+        ! @var : [reg_name] file name register to track all files declared
+        character(len=30),dimension(100) :: reg_name
     contains
         procedure,private :: initialize
+        procedure,private :: dump
         procedure ::  fout
         procedure ::  set_header
         procedure ::  get_header
@@ -23,15 +32,23 @@ module io
     ! @ interface : this interface bound constructor with type Ostream to mimic a constructor behavior
     interface Ostream 
         procedure :: constructor
+        procedure :: destructor
     end interface
 
 contains
     ! @func : same as C++ constructor
-    function constructor(string,port)
+    function constructor(string,port,namelst)
         type(Ostream) ::constructor
         character(len=*),intent(in)::string
         integer,intent(in) :: port(:)
-        call constructor%initialize(string,port)
+        character(len=*),dimension(:),intent(in),optional :: namelst
+
+        call constructor%initialize(string,port,namelst)
+    end function
+
+    function destructor()
+        type(Ostream) ::destructor
+        call destructor%dump()
     end function
 
     ! @func : getter
@@ -55,26 +72,60 @@ contains
     ! @func : used in constructor to intialized object 
     ! @param: [string] header appear at beginning
     ! @param: [port] which termianl to write output, 6 for stdout, other for file
-    subroutine initialize(this,string,port)
+    subroutine initialize(this,string,port,namelst)
         class (Ostream) ::  this
         character(len=*),intent(in)::string
         integer,intent(in) :: port(:)
-        integer :: tmp
+        character(len=*),dimension(:),intent(in),optional :: namelst
+        
+        integer :: tmp,i
+
         this%header='['//string//']'
         tmp = size(port)
         allocate(this%port(tmp))
         this%port=port
+        if(present(namelst)) then
+            if(size(namelst).ne.size(port)) then
+                print *,"IO Error, name list and port list not same length."
+                stop
+            else
+                allocate(this%namelst(tmp))
+                this%namelst = namelst
+                do i=1,tmp 
+                    print '("[io]  ",a,i3,3x,a5,a)','terminal',this%port(i)&
+                        &,'=>   ',this%namelst(i)
+                    !print *,'terminal',this%port(i),trim(this%namelst(i))
+                    !print *,trim(this%namelst(i))
+                    if((this%port(i).eq.6).and.(this%namelst(i).ne.'')) then
+                            print '("[io]  ",a)',"IO Error, port 6 &
+                                &is stdout cannot be assigned with file name."
+                    end if
+                end do
+            end if
+        end if
+        this%reg_name=''
     end subroutine initialize
 
+    subroutine dump(this)
+        class (Ostream) ::  this
+
+    end subroutine
     ! @func : output given string to designated port
     subroutine fout(this,string)
         class (Ostream),intent(in) ::  this
         character(len=*),intent(in)::string
         !character(len=*),optional::ctl
         integer :: i 
-        do i=1,size(this%port) 
-        write(this%port(i),fmt='(a)') this%header//' '//string
-    end do
+        if(allocated(this%namelst)) then
+            !do i=1,size(this%port) 
+                !write(this%port(i),fmt='(a)') this%header//' '//string
+            !end do
+        else
+            do i=1,size(this%port) 
+                write(this%port(i),fmt='(a)') this%header//' '//string
+            end do
+
+        end if
     end subroutine
    
     ! @func : [toString_int] convert a integer into string 
--- io/test.f90
+++ io/test.f90
@@ -1,6 +1,6 @@
 program tes
     use io
-    type(Ostream) :: test
+    type(Ostream) :: test,test2
     !real(4) a
     integer :: a
     !call test%initialize("test",6)
@@ -12,6 +12,8 @@ program tes
     call test%set_header('new')
     call test%fout(test%toString(a,'(i9)'))
     print *,test%get_header()
+    test2=Ostream("test",[6,13],['a.txt','b.txt'])
+
     !call test<<'2'
     !call test%fout(test%toString(a,'(f6.2)'),'2')
     !print *,test%toString(a)
